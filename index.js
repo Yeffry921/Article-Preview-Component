54e52ecfd3b4f76b5940f35cf2dd90cdd779ff68
yeffry <yeffry45@gmail.com>
2021-06-15 23:40:11 -0400

    add test component for active footer

---
+++ index.js
@@ -0,0 +1,21 @@
+const shareBtn = document.querySelector('.share');
+
+
+const shareComponent = `
+  <div class="share-footer">
+    <p>Share</p>
+
+    <img src="" class="" alt="">
+    <img src="" class="" alt="">
+    <img src="" class="" alt="">
+
+    <img src="./images/icon-share.svg" alt="" class="share">
+
+  </div>
+`
+
+// the challenge, general gist in 1-2 sentences
+
+// when a user clicks on the share btn the layout will change 
+// and the user will have the option to share from 3 different 
+// social media links
